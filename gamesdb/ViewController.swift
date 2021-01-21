27eda28397b1368cc8baa5961d2fc36be76f787b
Christoph Schwizer <christoph@schwizer.dev>
2021-01-21 13:28:58 +0100

    Initial Commit

---
+++ gamesdb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gamesdb
+//
+//  Created by Christoph Schwizer on 21.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
